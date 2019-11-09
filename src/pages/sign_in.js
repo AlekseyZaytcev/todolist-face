9ed7ebd00807fb88cbd883ed9aa30765b276a357
Aleksey Zaytcev <aleksey2142.zaytcev@gmail.com>
2019-11-09 22:30:52 +0200

    Update forms

--- src/pages/sign_in.js
+++ src/pages/sign_in.js
@@ -5,35 +5,60 @@ import { Form, Button } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SignInPage = () => (
-  <Layout pageInfo={{ pageName: "sign_in" }}>
-    <SEO title="SignIn" keywords={[`gatsby`, `react`, `bootstrap`]} />
+class SignInPage extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault()
+    const data = new FormData(e.target)
 
-    <Form className="form">
-      <h3 className="form_element">Sign In</h3>
-      <Form.Group controlId="formUserName">
-        <Form.Control size="lg" placeholder="User Name" />
-      </Form.Group>
+    fetch("https://todolist-endpoints.herokuapp.com/users/sign_in", {
+      method: "POST",
+      body: JSON.stringify({ user: Object.fromEntries(data) }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
 
-      <Form.Group controlId="formPassword">
-        <Form.Control size="lg" type="password" placeholder="Password" />
-      </Form.Group>
+  render() {
+    return (
+      <Layout pageInfo={{ pageName: "sign_in" }}>
+        <SEO title="SignIn" keywords={[`gatsby`, `react`, `bootstrap`]} />
 
-      <Button
-        size="lg"
-        className="form_element"
-        variant="primary"
-        type="submit"
-      >
-        Sign In
-      </Button>
-      <div>
-        <span>
-          Don't have an account? <Link to="/sign_up">Sign Up</Link>
-        </span>
-      </div>
-    </Form>
-  </Layout>
-)
+        <Form className="form" onSubmit={this.handleSubmit}>
+          <h3 className="form_element">Sign In</h3>
+          <Form.Group>
+            <Form.Control size="lg" name="username" placeholder="User Name" />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              size="lg"
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Button
+            className="form_element"
+            size="lg"
+            variant="primary"
+            type="submit"
+          >
+            Sign In
+          </Button>
+          <div>
+            <span>
+              Don't have an account? <Link to="/sign_up">Sign Up</Link>
+            </span>
+          </div>
+        </Form>
+      </Layout>
+    )
+  }
+}
 
 export default SignInPage
--- src/pages/sign_up.js
+++ src/pages/sign_up.js
@@ -5,43 +5,69 @@ import { Form, Button } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SignUpPage = () => (
-  <Layout pageInfo={{ pageName: "sign_up" }}>
-    <SEO title="SignUp" keywords={[`gatsby`, `react`, `bootstrap`]} />
+class SignUpPage extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault()
+    const data = new FormData(e.target)
 
-    <Form className="form">
-      <h3 className="form_element">Sign Up</h3>
-      <Form.Group controlId="formUserName">
-        <Form.Control size="lg" placeholder="User Name" />
-      </Form.Group>
+    fetch("https://todolist-endpoints.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify({ user: Object.fromEntries(data) }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
 
-      <Form.Group controlId="formPassword">
-        <Form.Control size="lg" type="password" placeholder="Password" />
-      </Form.Group>
+  render() {
+    return (
+      <Layout pageInfo={{ pageName: "sign_up" }}>
+        <SEO title="SignUp" keywords={[`gatsby`, `react`, `bootstrap`]} />
 
-      <Form.Group controlId="formConfirmPassword">
-        <Form.Control
-          size="lg"
-          type="password"
-          placeholder="Confirm Password"
-        />
-      </Form.Group>
+        <Form className="form" onSubmit={this.handleSubmit}>
+          <h3 className="form_element">Sign Up</h3>
+          <Form.Group>
+            <Form.Control size="lg" name="username" placeholder="User Name" />
+          </Form.Group>
 
-      <Button
-        size="lg"
-        className="form_element"
-        variant="primary"
-        type="submit"
-      >
-        Sign Up
-      </Button>
-      <div>
-        <span>
-          Allready a member? <Link to="/sign_in">Sign In</Link>
-        </span>
-      </div>
-    </Form>
-  </Layout>
-)
+          <Form.Group>
+            <Form.Control
+              size="lg"
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              size="lg"
+              name="password_confirmation"
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Form.Group>
+
+          <Button
+            size="lg"
+            className="form_element"
+            variant="primary"
+            type="submit"
+          >
+            Sign Up
+          </Button>
+          <div>
+            <span>
+              Allready a member? <Link to="/sign_in">Sign In</Link>
+            </span>
+          </div>
+        </Form>
+      </Layout>
+    )
+  }
+}
 
 export default SignUpPage
