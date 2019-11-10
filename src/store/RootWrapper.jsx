2f771486faf8149a0176d946982e5a1b9f0f5b20
Aleksey Zaytcev <aleksey2142.zaytcev@gmail.com>
2019-11-10 18:47:39 +0200

    Persist state to local storage

--- src/store/RootWrapper.jsx
+++ src/store/RootWrapper.jsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { Provider } from "react-redux"
-import { createStore } from "redux"
+import { compose, createStore } from "redux"
 import rootReducer from "./reducers"
+import persistState from "redux-localstorage"
+
+const enhancer = compose(persistState())
 
 export default ({ element }) => (
-  <Provider store={createStore(rootReducer)}>{element}</Provider>
+  <Provider store={createStore(rootReducer, {}, enhancer)}>{element}</Provider>
 )
