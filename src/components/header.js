0d7590ba309a52df03aa6bfb0899112a1cc5d98c
Aleksey Zaytcev <aleksey2142.zaytcev@gmail.com>
2019-11-12 01:14:55 +0200

    Add sign_out logic

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,67 @@
+import { navigate } from "gatsby"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { connect } from "react-redux"
+import { destroyAuthToken } from "../store/auth/actions"
+import { setWarningMessage } from "../store/messages/actions"
 
 import { Image } from "react-bootstrap"
 
-const Header = ({ siteTitle }) => (
-  <header className="header__background_color">
-    <div className="header__content_wrapper">
-      <Image src="icons/ruby-garage.svg" height="70" />
-      <div  className="header__content">
-        <h1>
-          <Link to="/">
-            {siteTitle}
-          </Link>
-        </h1>
-        <div className="header__control_buttons">
-          <a variant="dark" className="btn btn-secondary header__btn" href="/sign_in">Sign In</a>
-          <a variant="dark" className="btn btn-secondary header__btn" href="/sign_up">Sign Up</a>
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSignOutClick = this.handleSignOutClick.bind(this)
+  }
+
+  handleSignOutClick() {
+    this.props.destroyAuthToken()
+    this.props.setWarningMessage("You are signed out!")
+    navigate("/sign_in")
+  }
+
+  render() {
+    return (
+      <header className="header__background_color">
+        <div className="header__content_wrapper">
+          <Image src="icons/ruby-garage.svg" height="70" />
+          <div className="header__content">
+            <h1>
+              <Link to="/">{this.props.siteTitle}</Link>
+            </h1>
+            <div className="header__control_buttons">
+              {this.props.authToken ? (
+                <Image
+                  src="icons/logout.svg"
+                  className="header__btn"
+                  height="40"
+                  onClick={this.handleSignOutClick}
+                />
+              ) : (
+                <div>
+                  <a
+                    variant="dark"
+                    className="btn btn-secondary header__btn"
+                    href="/sign_in"
+                  >
+                    Sign In
+                  </a>
+                  <a
+                    variant="dark"
+                    className="btn btn-secondary header__btn"
+                    href="/sign_up"
+                  >
+                    Sign Up
+                  </a>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </header>
-)
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -31,4 +71,13 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+const mapStateToProps = state => {
+  return { authToken: state.auth.token }
+}
+
+const mapDispatchToProps = { destroyAuthToken, setWarningMessage }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header)
